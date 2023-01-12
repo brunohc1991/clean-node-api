7884ba2abc2b4ad96c700f40711722bb378d1e63
brunocosta1991 <bruno.costa@velti.com.br>
2023-01-12 07:39:12 -0300

    feat: ensure LoginController returns 500 if emailValidator throws

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
-import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest } from '../../helpers/http-helper'
+import { InvalidParamError, MissingParamError, ServerError } from '../../errors'
+import { badRequest, serverError } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '../../protocols'
 import { EmailValidator } from '../signup/signup-protocols'
 
@@ -10,21 +10,22 @@ export class LoginController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredFieds = ['email', 'password']
+    try {
+      const requiredFieds = ['email', 'password']
 
-    for (const field of requiredFieds) {
-      if (!httpRequest.body[field]) {
-        return await new Promise(resolve => resolve(badRequest(new MissingParamError(field))))
+      for (const field of requiredFieds) {
+        if (!httpRequest.body[field]) {
+          return await new Promise(resolve => resolve(badRequest(new MissingParamError(field))))
+        }
       }
+      const { email } = httpRequest.body
+      const isEmailValid = this.emailValidator.isValid(email)
+      if (!isEmailValid) {
+        return await new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
+      }
+      return await new Promise(resolve => resolve(badRequest(new MissingParamError('email')))) 
+    } catch (error) {
+      return serverError(error)
     }
-
-    const { email } = httpRequest.body
-
-    const isEmailValid = this.emailValidator.isValid(email)
-    if (!isEmailValid) {
-      return await new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
-    }
-
-    return await new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
   }
 }

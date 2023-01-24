9ce48470efd909b8950a832b7723635475551a1c
brunocosta1991 <bruno.costa@velti.com.br>
2023-01-24 08:00:05 -0300

    refactor: move HttpHelper to a folder

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,6 +1,6 @@
 import { Authentication, HttpRequest } from './login-protocols'
 import { MissingParamError, ServerError } from '../../errors'
-import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper'
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper'
 import { LoginController } from './login'
 import { Validation } from '../signup/signup-protocols'
 

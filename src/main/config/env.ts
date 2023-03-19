6ac8f3fe9124748509ce6ae24fa4f796451e9255
Bruno H Costa <brunohc1991@gmail.com>
2023-03-19 16:18:58 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL as string || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT as string || 5050
+  port: process.env.PORT as string || 5050,
+  jwtSecret: process.env.jwtSecret as string || 'ç@3sdcÇ4'
 }

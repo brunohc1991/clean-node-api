cbb2b309d5049ddccd55ebae9c9d699c4fdeb399
Bruno H Costa <brunohc1991@gmail.com>
2023-03-19 17:12:18 -0300

    refactor: change addres database local

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL as string || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL as string || 'mongodb://127.0.0.1:27017/clean-node-api',
   port: process.env.PORT as string || 5050,
   jwtSecret: process.env.jwtSecret as string || 'ç@3sdcÇ4'
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -3,5 +3,5 @@ import env from './config/env'
 
 void MongoHelper.connect(env.mongoUrl).then(async () => {
   const app = (await import ('./config/app')).default
-  app.listen(env.port, () => console.log(`server running at http://localhost:${env.port}`))
+  app.listen(env.port, () => console.log(`server running at http://127.0.0.1:${env.port}`))
 }).catch(console.error)

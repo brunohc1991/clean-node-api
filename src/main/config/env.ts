0bd6bdeefc0a76a8e9ee0c02733f76a88278aeb6
Bruno H Costa <brunohc1991@gmail.com>
2023-03-27 23:41:31 -0300

    chore: add engines with node version

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL as string || 'mongodb://127.0.0.1:27017/clean-node-api',
   port: process.env.PORT as string || 5050,
-  jwtSecret: process.env.jwtSecret as string || 'ç@3sdcÇ4'
+  jwtSecret: process.env.JWT_SECRET as string || 'ç@3sdcÇ4'
 }

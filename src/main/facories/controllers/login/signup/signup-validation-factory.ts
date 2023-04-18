dd5d14f28adccfca91640474b0b8838d24e8dbf4
Bruno H Costa <brunohc1991@gmail.com>
2023-04-18 08:06:50 -0300

    refactor: organize main layer folders

--- src/main/facories/controllers/login/signup/signup-validation-factory.ts
+++ src/main/facories/controllers/login/signup/signup-validation-factory.ts
@@ -1,6 +1,6 @@
-import { CompareFieldValidation, EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators'
-import { Validation } from '../../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator-adapter'
+import { CompareFieldValidation, EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../../validation/validators'
+import { Validation } from '../../../../../presentation/protocols/validation'
+import { EmailValidatorAdapter } from '../../../../../infra/validators/email-validator-adapter'
 
 export const makeSignupValidation = (): ValidationComposite => {
   const validations: Validation[] = []

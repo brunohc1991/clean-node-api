48f5786ec4f786310ec3494663997dadd5d51b21
Bruno H Costa <brunohc1991@gmail.com>
2023-04-22 06:42:45 -0300

    feat: ensure LoadSurveysRoutes return 403 if no accessToken is provided

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -3,9 +3,14 @@ import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddSurveyController } from '../facories/controllers/survey/add-survey/add-survey-controller-factory'
 import { makeAuthMiddleware } from '../facories/middlewares/auth-middleware-factory'
 import { adaptMiddleware } from '../adapters/express-middleware-adapter'
+import { makeLoadSurveysController } from '../facories/controllers/survey/load-surveys/load-surveys-controller-factory'
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+  const auth = adaptMiddleware(makeAuthMiddleware())
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
+
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  router.get('/surveys', auth, adaptRoute(makeLoadSurveysController()))
 }

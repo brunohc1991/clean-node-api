88228e042b8124f0c6f220ac0e503579416c6733
Bruno H Costa <brunohc1991@gmail.com>
2023-04-22 09:27:17 -0300

    refactor: change all paths to use modules

--- src/presentation/controllers/login/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login/login-controller.spec.ts
@@ -1,6 +1,6 @@
 import { Authentication, AuthenticationModel, HttpRequest } from './login-controller-protocols'
-import { MissingParamError, ServerError } from '../../../errors'
-import { badRequest, ok, serverError, unauthorized } from '../../../helpers/http/http-helper'
+import { MissingParamError, ServerError } from '@/presentation/errors'
+import { badRequest, ok, serverError, unauthorized } from '@/presentation/helpers/http/http-helper'
 import { LoginController } from './login-controller'
 import { Validation } from '../signup/signup-controller-protocols'
 

c62e1b422ab984d25a20916a07ae8af721a0a83d
Bruno H Costa <brunohc1991@gmail.com>
2023-04-30 17:12:29 -0300

    feat: ensure SaveSurveyResultRoutes returns 403 if no token is provided

---
+++ src/main/routes/survey-result-routes.ts
@@ -0,0 +1,9 @@
+import { makeSaveSurveyResultController } from '../factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
+import { Router } from 'express'
+import { adaptRoute } from '../adapters/express-route-adapter'
+import { auth } from '../middlewares/auth'
+
+export default (router: Router): void => {
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
+}

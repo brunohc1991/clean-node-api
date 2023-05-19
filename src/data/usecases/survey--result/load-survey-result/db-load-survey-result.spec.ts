67473909b4e3402a3e5c81f974225dba1fc01f2d
Bruno H Costa <brunohc1991@gmail.com>
2023-05-19 07:54:20 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/survey--result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey--result/load-survey-result/db-load-survey-result.spec.ts
@@ -3,15 +3,30 @@ import { SurveyResultModel } from '../save-survey-result/db-save-survey-result-p
 import { DbLoadSurveyResult } from './db-load-survey-result'
 import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
 
+type SutTypes = {
+  sut: DbLoadSurveyResult
+  loadSurveyResultRepositoryStub: LoadSurveyResultRepository
+}
+
+const mockLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
+  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+    async loadBySurveyId (surveyId: String): Promise<SurveyResultModel | null> {
+      return await Promise.resolve(mockSurveyResultModel())
+    }
+  }
+  return new LoadSurveyResultRepositoryStub()
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultRepositoryStub = mockLoadSurveyResultRepository()
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+
+  return { sut, loadSurveyResultRepositoryStub }
+}
+
 describe('DbLoadSurveyResult UseCase', () => {
   test('Should call loadSurveyResultRepository', async () => {
-    class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
-      async loadBySurveyId (surveyId: String): Promise<SurveyResultModel | null> {
-        return await Promise.resolve(mockSurveyResultModel())
-      }
-    }
-    const loadSurveyResultRepositoryStub = new LoadSurveyResultRepositoryStub()
-    const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
     const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
     await sut.load('any_survey_id')
     expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id')

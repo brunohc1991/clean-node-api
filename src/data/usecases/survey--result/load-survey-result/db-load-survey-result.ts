964637a2f12116e31029e3d70cea98c1b5080a61
Bruno H Costa <brunohc1991@gmail.com>
2023-05-25 08:26:32 -0300

    feat: ensure DbLoadSurveyResult returns empty survey result if there is no result

--- src/data/usecases/survey--result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey--result/load-survey-result/db-load-survey-result.ts
@@ -4,9 +4,20 @@ export class DbLoadSurveyResult implements LoadSurveyResult {
   constructor (private readonly loadSurveyResultRepository: LoadSurveyResultRepository, private readonly loadSurveyByIdRepository: LoadSurveyByIdRepository) {}
 
   async load (surveyId: string): Promise<SurveyResultModel | null> {
-    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    let surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
     if (!surveyResult) {
-      await this.loadSurveyByIdRepository.loadById(surveyId)
+      const survey = await this.loadSurveyByIdRepository.loadById(surveyId)
+      if (survey) {
+        surveyResult = {
+          surveyId: survey.id,
+          question: survey.question,
+          date: survey.date,
+          answers: survey.answers.map(answer => Object.assign({}, answer, {
+            count: 0,
+            percent: 0
+          }))
+        }
+      }
     }
     return surveyResult
   }

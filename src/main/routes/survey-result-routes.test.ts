a29d43a9598ae5550e1914899bf6404b07dff31f
Bruno H Costa <brunohc1991@gmail.com>
2023-05-27 09:12:23 -0300

    feat: ensure LoadSurveyRoot returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -80,5 +80,26 @@ describe('Survey Routes', () => {
         .get('/api/surveys/any_id/results')
         .expect(403)
     })
+
+    test('Should return 200 on load survey result with accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'question',
+        answers: [{
+          answer: 'answer1',
+          image: 'http://image-name.com'
+        },
+        {
+          answer: 'answer2'
+        }],
+        date: new Date()
+      })
+
+      await request(app)
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        .get(`/api/surveys/${res.ops[0]._id}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })

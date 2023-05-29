d5585b9b3c557a15c61c2e30aa0425521afece1e
Bruno H Costa <brunohc1991@gmail.com>
2023-05-29 07:48:20 -0300

    fix: ensure DbLoadAccountByToken return null if decrypter throws

--- src/data/usecases/account/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/account/load-account-by-token/db-load-account-by-token.ts
@@ -4,7 +4,12 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
   constructor (private readonly decrypter: Decrypter, private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository) {}
 
   async load (accessToken: string, role?: string | undefined): Promise<AccountModel | null> {
-    const token = await this.decrypter.decrypt(accessToken)
+    let token: string | null
+    try {
+      token = await this.decrypter.decrypt(accessToken)
+    } catch (error) {
+      return null
+    }
     if (token) {
       const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
       if (account) {
